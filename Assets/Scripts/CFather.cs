0e7d6057a587424de03f120539f95729b97cbd75
SenorConeho <arcoelho01@>
2014-01-26 12:54:09 -0200

    Adicionando a mecanica do pai

---
+++ Assets/Scripts/CFather.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class name and description
+/// </summary>
+public class CFather : MonoBehaviour {
+
+	/* ==========================================================================================================
+	 * CLASS VARIABLES
+	 * ==========================================================================================================
+	 */
+	// PUBLIC
+	public Texture2D tFatherText;
+
+	// PRIVATE
+
+	// PROTECTED
+
+	/* ==========================================================================================================
+	 * UNITY METHODS
+	 * ==========================================================================================================
+	 */
+	//
+	void Awake() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Physics
+	void FixedUpdate() {
+
+	}
+
+	/* ==========================================================================================================
+	 * CLASS METHODS
+	 * ==========================================================================================================
+	 */
+	void OnTriggerEnter2D(Collider2D col) {
+
+		if(col.gameObject.layer == 9) {
+
+			// Hit a player. Get the parent from the parent of the collider
+			Debug.Log("Father: " + col.transform);
+		}
+	}
+}
