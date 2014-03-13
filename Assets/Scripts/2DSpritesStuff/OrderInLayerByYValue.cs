d243062a58b1e1b11a901bd3a32580cb61404632
SenorConeho <senorconeho@gmail.com>
2014-03-13 17:51:58 -0300

    Adicionado script para ordernar objetos
    Objetos com este script ficam ordenados automaticamente no seu layer
    conforme sua posição y

---
+++ Assets/Scripts/2DSpritesStuff/OrderInLayerByYValue.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script 'auto-assign' the 'order in layer' value accordingly to it's y position in the world. This way
+/// we guarantee that a object 'up' (higher y value) in the scene will be drawn behind a object 'down' 
+/// (smaller y value) in the scene
+/// </summary>
+public class OrderInLayerByYValue : MonoBehaviour {
+
+	/* ==========================================================================================================
+	 * CLASS VARIABLES
+	 * ==========================================================================================================
+	 */
+	// PUBLIC
+	public int nMaxOrderValue = 20;
+	public Transform	trSpriteObject;	//< Object that contains the sprite renderer component
+
+	// PRIVATE
+	private SpriteRenderer spriteRenderer;
+
+	// PROTECTED
+
+	/* ==========================================================================================================
+	 * UNITY METHODS
+	 * ==========================================================================================================
+	 */
+	//
+	void Awake() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+		if(trSpriteObject == null) {
+
+			trSpriteObject = this.transform;
+		}
+
+		spriteRenderer = trSpriteObject.gameObject.GetComponent<SpriteRenderer>();
+
+		// Change the sprite renderer according to the object y position in the world
+		int nNewLayerOrder = nMaxOrderValue - (int) trSpriteObject.position.y;
+
+		if(nNewLayerOrder < 0 )
+			nNewLayerOrder = 0;
+
+		spriteRenderer.sortingOrder = nNewLayerOrder;
+	}
+}
